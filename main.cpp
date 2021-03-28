b924c88c89981dc694cb1b1d7f4e3feb0ed606ac
Raspy-Py <naumenko.roman2003@gmail.com>
2021-03-28 15:26:20 +0300

    Add stack destructor and constructors

--- main.cpp
+++ main.cpp
@@ -26,3 +26,30 @@ int main() {
     return 0;
 }
 
+
+stack::stack (char * input_string)
+{
+    int n = strlen(input_string);
+
+    tokens = new char[n];
+
+    stack_current_size = 0;
+
+    for (size_t i = 0; i < n; i++, stack_current_size++)
+    {
+        tokens[stack_current_size] = input_string[n - 1 - i];
+    }
+}
+
+stack::stack()
+{
+    stack_current_size = 0;
+    tokens = new char[100];
+}
+
+stack ::~stack ()
+{
+    delete[] tokens;
+}
+
+
