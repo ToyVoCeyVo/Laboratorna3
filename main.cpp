addf09a06db639d3185a0c1635d803480d4d4b97
Raspy-Py <naumenko.roman2003@gmail.com>
2021-03-28 22:37:08 +0300

    Removed stolen Shunting Yard

--- main.cpp
+++ main.cpp
@@ -27,18 +27,15 @@ private:
 };
 
 void read_args(int, char**, char*);
-string shuntingYard(const string& input);
-unsigned op_preced(const char ch);
-bool is_number(char ch);
-bool is_operator(char ch);
+
 
 
 //---Main function---
 
 int main(int argc, char* argv[]) {
 
-    char expresion[100] = "";
-
+    char expresion[100] = "1+3";
+/*
     if(argc > 1) {
         read_args(argc, argv, expresion);
         cout << "Your expresion: " << expresion << endl; // GET RID OF in release!
@@ -46,11 +43,10 @@ int main(int argc, char* argv[]) {
         cout << "Run program from console!\n";
         return 1;
     }
+*/
 
     string input(expresion);
     string output;
-    output = shuntingYard(input);
-    cout << "Polish notation: " << output << endl;
 
     return 0;
 }
@@ -113,90 +109,3 @@ void read_args(int argc, char** argv, char* expr){
     }
 }
 
-//алгоритм сортувальної станції
-string shuntingYard(const string& input)
-{
-    stack operators;
-    string output("");
-    unsigned length = input.length();
-    for (unsigned i = 0; i < length; ++i)
-    {
-        //перевірка чи вхідний символ є частиною числа(врахування унарного  '-')
-        if (is_number(input[i]) || (input[i] == '-' && i == 0) || (i > 0 && input[i-1] == '(' && input[i] == '-'))
-        {
-            //перевірка це останній символ числа,щоб поставити після нього пробіл
-            if(i + 1 == length || is_operator(input[i+1]) || input[i+1] == '(' || input[i+1] == ')')
-            {
-                output = output + input[i] + ' ';
-            }
-            else
-            {
-                output = output + input[i];
-            }
-        }
-        else if (input[i] == '(')
-        {
-            operators.push_back('(');
-        }
-        else if (input[i] == ')')
-        {
-            while (operators.show_last() != '(')
-            {
-                output = output + operators.pop()+ ' ';
-            }
-            operators.pop();
-        }
-        else if (is_operator(input[i]))
-        {
-            while (!operators.size()==0 && (op_preced(operators.show_last()) >= op_preced(input[i])))
-            {
-                output = output + operators.pop() + ' ';
-            }
-            operators.push_back(input[i]);
-        }
-        else
-        {
-            //помилка вхідних даних
-            throw "Помилка вхідних даних";
-        }
-    }
-    //виводимо символи що залишились у стеку
-    unsigned stackSize = operators.size();
-    if (stackSize > 0)
-    {
-        for (unsigned i = 0; i < stackSize - 1; ++i)
-        {
-            output = output + operators.pop() + ' ';
-        }
-        output = output + operators.pop();
-    }
-    return output;
-}
-
-
-unsigned op_preced(const char ch)
-{
-    switch(ch)
-    {
-        case '^':
-            return 3;
-        case '*':
-        case '/':
-            return 2;
-        case '+':
-        case '-':
-            return 1;
-    }
-    // для випадку вхідного символу '('
-    return 0;
-}
-
-bool is_number(char ch)
-{
-    return (('0' <= ch) && (ch <= '9')) || (ch == '.');
-}
-
-bool is_operator(char ch)
-{
-    return (ch == '+' || ch == '-' || ch == '/' || ch == '*' || ch == '^');
-}
