99e9bcc6c212cdaecce5a6f9d2250ffd2c2d846c
Raspy-Py <naumenko.roman2003@gmail.com>
2021-03-28 15:17:26 +0300

    Add stack class' prototype

--- main.cpp
+++ main.cpp
@@ -4,6 +4,23 @@
 
 using  namespace std ;
 
+class stack
+{
+public:
+    stack (char*);
+    stack();
+    ~stack ();
+
+    char show_last();
+    char pop();
+    void push_back(char);
+    int size();
+
+private:
+    char* tokens;
+    int stack_current_size;
+};
+
 int main() {
 
     return 0;
